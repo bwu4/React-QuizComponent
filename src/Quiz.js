60f8ead01fb31bff59d276ab843050e9ff00523a
brian wu <bwu4@slb.com>
2018-08-03 15:08:21 -0500

    Creating a Quiz Component1

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component {
     }
     render() {
         return (
-            <div><div className="QuizQuestions">{quizData.quiz_questions[0].instruction_text}</div></div>
+            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
         )
     }
 }
